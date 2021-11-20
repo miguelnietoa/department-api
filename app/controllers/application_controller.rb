fdce34997d9e8a75394bbc002c7d11ccddac2aa0
Miguel Nieto A <anietom@uninorte.edu.co>
2021-11-20 17:01:36 -0500

    feat: add index endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  include Response
+
+  def index
+    json_response(message: 'Hello World!')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # department param can ends with dot (.), so we need to add a constraint using regexp
   get '/departments/:department/fix', to: 'departments#fix', constraints: { department: /.*\.?/ }
+
+  # Index endpoint for the API
+  get '/', to: 'application#index'
 end
